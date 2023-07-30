11da0016d3c4d334366fde44757a8d105fa90754
Ngo Minh Phuong <minhphuong49th@gmail.com>
2023-07-30 14:46:29 +0700

    edit code

--- TF-infrastructure/modules/networking/vpc/output_vpc.tf
+++ TF-infrastructure/modules/networking/vpc/output_vpc.tf
@@ -1,23 +1,23 @@
-# output "vpc" { 
-#   value = aws_vpc.vpc.id
-#   }
+output "vpc" { 
+  value = aws_vpc.vpc.id
+  }
 
-# output "public_subnet" {
-#   value = aws_subnet.public_subnet[*].id
-# }
+output "public_subnet" {
+  value = aws_subnet.public_subnet[*].id
+}
 
-# output "private_subnet" {
-#   value = aws_subnet.private_subnet[*].id
-# }
+output "private_subnet" {
+  value = aws_subnet.private_subnet[*].id
+}
 
-# output "ElasticIP" {
-#     value = aws_eip.elasticeip.public_ip
-# }
+output "ElasticIP" {
+    value = aws_eip.elasticeip.public_ip
+}
 
-# output "aws_account_id" {
-#     value = aws_subnet.public_subnet[0].owner_id
-# }
+output "aws_account_id" {
+    value = aws_subnet.public_subnet[0].owner_id
+}
 
-# output "public_subnet_arn" {
-#     value = aws_subnet.public_subnet[*].arn
-# }
+output "public_subnet_arn" {
+    value = aws_subnet.public_subnet[*].arn
+}
--- TF-infrastructure/modules/networking/vpc/variable.tf
+++ TF-infrastructure/modules/networking/vpc/variable.tf
@@ -1,31 +1,31 @@
-# variable "project" {
-#   type    = string
-# }
-# variable "vpc_cidr_block" {
-#   type    = string
-# }
+variable "project" {
+  type    = string
+}
+variable "vpc_cidr_block" {
+  type    = string
+}
 
-# variable "private_subnet" {
-#   type    = list(string)
-# }
+variable "private_subnet" {
+  type    = list(string)
+}
 
-# variable "public_subnet" {
-#   type    = list(string)
-# }
+variable "public_subnet" {
+  type    = list(string)
+}
 
-# variable "rds_subnet" {
-#   type    = list(string)
-# }
-# variable "availability_zone" {
-#   type    = list(string)
-# }
-# variable "region" {
-#   type    = string
-# }
+variable "rds_subnet" {
+  type    = list(string)
+}
+variable "availability_zone" {
+  type    = list(string)
+}
+variable "region" {
+  type    = string
+}
 
-# variable "tags_name" {
-#   type = map(string)
-# }
+variable "tags_name" {
+  type = map(string)
+}
 
 
 
