99cd70cd62e2221b9a8d3c8e508e89a0643ba68c
Ngo Minh Phuong <minhphuong49th@gmail.com>
2023-07-30 14:17:49 +0700

    push code

--- TF-alb-instances-sg/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "tags_name" {}
-variable "instance_type" {}
-variable "loadbalancer_name" {}
-variable "target_group_name" {} 
-# variable "vpc_id" {} 
-# variable "subnets" {} 
-# variable "subnet_id" {} 
